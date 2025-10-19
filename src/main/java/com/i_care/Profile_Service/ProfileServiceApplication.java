fe3acac11bd532ff296d9a260f10b689cd64f378
shyamsah23 <92447257+shyamsah23@users.noreply.github.com>
2025-10-19 22:19:17 +0530

    Profile MS

---
+++ src/main/java/com/i_care/Profile_Service/ProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.i_care.Profile_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/i_care/Profile_Service/ProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i_care.Profile_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
