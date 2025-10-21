539f38e5d5e9f2bfa2433841e09336bc4c522615
jhaayush11 <jhanathayush@gmail.com>
2025-10-22 00:41:36 +0530

    Loggers and constants added to Profile service

--- src/main/java/com/i_care/Profile_Service/controller/PatientController.java
+++ src/main/java/com/i_care/Profile_Service/controller/PatientController.java
@@ -3,27 +3,39 @@ package com.i_care.Profile_Service.controller;
 import com.i_care.Profile_Service.dto.PatientDTO;
 import com.i_care.Profile_Service.exception.ProfileException;
 import com.i_care.Profile_Service.service.PatientService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/profile/patient")
 @Validated
 public class PatientController {
+
+    Logger logger = LoggerFactory.getLogger(PatientController.class);
+
     @Autowired
     private PatientService patientService;
 
     @PostMapping("/add")
     public ResponseEntity<Long> addPatient(@RequestBody PatientDTO patientDTO) throws ProfileException {
-        return new ResponseEntity<>(patientService.addPatient(patientDTO), HttpStatus.CREATED);
+        logger.info("Adding Patient - name = {}", patientDTO.getName());
+        Long id = patientService.addPatient(patientDTO);
+        logger.info("Successfully Added Patient to the Record");
+        return new ResponseEntity<>(id, HttpStatus.CREATED);
     }
 
     @GetMapping("/get/{id}")
     public ResponseEntity<PatientDTO> getPatientById(@PathVariable Long id) throws ProfileException {
-        return new ResponseEntity<>(patientService.getPatientById(id), HttpStatus.CREATED);
+        logger.info("Getting Patient info by id = {}",id);
+        PatientDTO patientDetails=patientService.getPatientById(id);
+        logger.info("Fetched Patient Details Successfully");
+        return new ResponseEntity<>(patientDetails,HttpStatus.FOUND);
     }
 }
--- src/main/java/com/i_care/Profile_Service/service/PatientServiceImpl.java
+++ src/main/java/com/i_care/Profile_Service/service/PatientServiceImpl.java
@@ -1,29 +1,52 @@
 package com.i_care.Profile_Service.service;
 
 import com.i_care.Profile_Service.dto.PatientDTO;
+import com.i_care.Profile_Service.entity.Doctor;
+import com.i_care.Profile_Service.entity.Patient;
 import com.i_care.Profile_Service.exception.ProfileException;
 import com.i_care.Profile_Service.repository.PatientRepository;
+import com.i_care.Profile_Service.utility.ProfileConstants;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PatientServiceImpl implements PatientService {
 
+    Logger logger = LoggerFactory.getLogger(PatientServiceImpl.class);
+
     @Autowired
     PatientRepository patientRepository;
 
     @Override
     public Long addPatient(PatientDTO patientDTO) throws ProfileException {
-        if (patientDTO.getEmail() != null && patientRepository.findByEmail(patientDTO.getEmail()).isPresent())
-            throw new ProfileException("PATIENT_ALREADY_EXISTS");
-        if (patientDTO.getAadhaarNo() != null && patientRepository.findByAadhaarNo(patientDTO.getAadhaarNo()).isPresent())
-            throw new ProfileException("PATIENT_ALREADY_EXISTS");
+        logger.info("Checking if Patient with name = {} & other provided details exists already or not",patientDTO.getName());
+        if (patientDTO.getEmail() != null && patientRepository.findByEmail(patientDTO.getEmail()).isPresent()){
+            logger.info(" Patient already exixts");
+            throw new ProfileException(ProfileConstants.PATIENT_ALREADY_EXISTS);
+        }
+
+        if (patientDTO.getAadhaarNo() != null && patientRepository.findByAadhaarNo(patientDTO.getAadhaarNo()).isPresent()){
+            logger.info(" Patient already exixts");
+            throw new ProfileException(ProfileConstants.PATIENT_ALREADY_EXISTS);
+        }
+        logger.info(" Patient name = {} saved to the system",patientDTO.getName());
         return patientRepository.save(patientDTO.toEntity()).getId();
     }
 
     @Override
     public PatientDTO getPatientById(Long id) throws ProfileException {
-        return patientRepository.findById(id).orElseThrow(() -> new ProfileException("PATIENT_NOT_FOUND")).toDTO();
+        Patient patient = patientRepository.findById(id).orElse(null);
+        logger.info("Trying to find Patient with id = {} in system",id);
+        if (patient == null) {
+            logger.info("Patient with id = {} not found in system",id);
+            throw new ProfileException(ProfileConstants.PATIENT_NOT_FOUND);
+        } else {
+            logger.info("Patient with id = {} found in system",id);
+            return patient.toDTO();
+        }
     }
 
 }
