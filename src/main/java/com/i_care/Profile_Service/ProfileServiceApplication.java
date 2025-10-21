04649d943b74b240bd4fe5b2f93ec92ea3bbe741
jhaayush11 <jhanathayush@gmail.com>
2025-10-22 00:52:33 +0530

    A comment resolved

--- src/main/java/com/i_care/Profile_Service/ProfileServiceApplication.java
+++ src/main/java/com/i_care/Profile_Service/ProfileServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProfileServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("This is profile service");
 		SpringApplication.run(ProfileServiceApplication.class, args);
 	}
 
