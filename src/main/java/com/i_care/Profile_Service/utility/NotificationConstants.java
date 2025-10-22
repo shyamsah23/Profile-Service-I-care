69b3c5970c1631941c3586af1a0d2529fd6208c2
shyamsah23 <92447257+shyamsah23@users.noreply.github.com>
2025-10-22 21:48:14 +0530

    Constant Fix

--- src/main/java/com/i_care/Profile_Service/utility/NotificationConstants.java
+++ src/main/java/com/i_care/Profile_Service/utility/NotificationConstants.java
@@ -10,5 +10,5 @@ public class NotificationConstants {
     public static final String DOCTOR_REGISTER = "DOCTOR_REGISTER";
     public static final String PATIENT_REGISTER = "PATIENT_REGISTER";
     public static final String DOCTOR_REGISTER_SUBJECT = "Congratulations! Your i-Care Doctor Account is Ready";
-    public static final String PATIENT_REGISTER_SUBJECT = "Congratulations! Your i-Care Doctor Account is Ready";
+    public static final String PATIENT_REGISTER_SUBJECT = "Congratulations! Your i-Care Patient Account is Ready";
 }
